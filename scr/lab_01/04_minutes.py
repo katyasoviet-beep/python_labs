8999ea90bce0c1136b99e042a403baaeec1a01f2
katyasoviet-beep <katyasoviet@gmail.com>
2025-10-01 00:47:50 +0300

    Create 04_minutes.py

---
+++ scr/lab_01/04_minutes.py
@@ -0,0 +1,11 @@
+m=int(input('Введите минуты:'))
+h=m//60
+m1=m%60
+if h >= 24:
+    d=h//24
+    h1=h%24
+    print(m)
+    print(d, h1, m1, sep=':')
+else:
+    print(m)
+    print(h, m1, sep=':')
